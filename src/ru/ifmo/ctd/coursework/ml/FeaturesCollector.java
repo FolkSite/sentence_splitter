98bc0229b0a428e0a60ba25dc9f4540aa45a7aab
Ioann Volkov <volkov.ioann@gmail.com>
2013-01-22 16:43:26 +0400

    First version of features collector

---
+++ src/ru/ifmo/ctd/coursework/ml/FeaturesCollector.java
@@ -0,0 +1,72 @@
+package ru.ifmo.ctd.coursework.ml;
+
+import java.io.*;
+import java.util.*;
+
+import ru.ifmo.ctd.coursework.ml.svm.Test;
+
+public class FeaturesCollector {
+	private ArrayList<Test> featuresCollection;
+
+	public FeaturesCollector() {
+		featuresCollection = new ArrayList<Test>();
+	}
+	
+	public FeaturesCollector(String inputFileName) {
+		featuresCollection = new ArrayList<Test>();
+		parsePunctuationMarksFromFile(inputFileName);
+	}
+	
+	public void parsePunctuationMarksFromFile(String fileName) {
+		try {
+			Scanner scanner = new Scanner(new BufferedInputStream(new FileInputStream(new File(fileName))), "UTF-8");
+			String fileContent = scanner.useDelimiter("\\Z").next();  // Reading whole file
+			parseEachPunctuationMark(fileContent);
+		} catch (IOException e) {
+			System.err.println("Error: could not open input file");
+			e.printStackTrace();
+		}
+	}
+	
+	private boolean isPunctuationMark(char c) {
+		return (c == '.' || c == '?' || c == '!' || c == ',' || c == ':' ||
+		        c == ';' || c == '\'' || c == '"' || c == '-');
+	}
+	
+	// Expecting that there is line ending between sentences and no other line endings in s
+	public void parseEachPunctuationMark(String s) {
+		for (int i = 1; i < s.length(); ++i) {
+			char c = s.charAt(i);
+			if (c == '.' || c == '?' || c == '!') {
+				int y = (i + 1 == s.length() || s.charAt(i + 1) == '\n' || s.charAt(i + 1) == '\r') ? 1 : -1;
+				
+				final int FEATURES_COUNT = 4;
+				double features[] = new double[FEATURES_COUNT];
+
+				int cur = 0;  // Current feature number
+				
+				// 1. Space on the left
+				features[cur] = (s.charAt(i - 1) == ' ') ? 1.0 : -1.0;
+				++cur;
+
+				// 2. Space on the right (end of line counting as space, it was needed only to know correct answer)
+				features[cur] = (i + 1 == s.length() || s.charAt(i + 1) == ' ' || s.charAt(i + 1) == '\n' || s.charAt(i + 1) == '\r') ? 1.0 : -1.0;
+				++cur;
+				
+				// 3. Punctuation mark on the left
+				features[cur] = isPunctuationMark(s.charAt(i - 1)) ? 1.0 : -1.0;
+				++cur;
+				
+				// 4. Punctuation mark on the right
+				features[cur] = (i + 1 < s.length() && isPunctuationMark(s.charAt(i + 1))) ? 1.0 : -1.0;
+				++cur;
+				
+				featuresCollection.add(new Test(features, FEATURES_COUNT, y));
+			}
+		}
+	}
+	
+	public ArrayList<Test> getFeaturesCollection() {
+		return featuresCollection;
+	}
+}
